9360f499fd0e3607e63d7f63230df0eec82f3c9a
Terrence Kuiper <github@terrencekuiper.com>
2019-11-22 19:10:20 +0000

    Add stoybook

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+require("dotenv").config();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/ping", function(req, res) {
+  return res.send("pong");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
 
 console.log("Listening on localhost:8080");
+
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
