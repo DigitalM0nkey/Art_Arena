53774c9fb3bd6af857f30c6306f2614bbcc681cf
Terrence Kuiper <github@terrencekuiper.com>
2019-11-22 23:16:54 +0000

    Update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
 import { configure } from "@storybook/react";
 
+function loadStories() {
+  require("../stories");
+}
+
+configure(loadStories, module);
+
 // automatically import all files ending in *.stories.js
-configure(require.context("../stories", true, /\.stories\.js$/), module);
+// configure(require.context("../stories", true, /\.stories\.js$/), module);
